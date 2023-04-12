bccb51eb5d7cde88e9430d9f752224ee77d1af79
Erik M. Dodd <emdodd27@gmail.com>
2023-04-12 12:05:35 -0700

    Fixed some CSS styling things. Changed the nav a bit

--- src/pages/Clients.js
+++ src/pages/Clients.js
@@ -1,46 +1,39 @@
 import React from 'react';
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  margin-top: 800px; /* add margin to the top */
+`;
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ListItem = styled.li`
+  display: block;
+`;
 
 const Clients = () => {
-    const clients = [
-        'Organic by John Patrick',
-        'Caron Callahan',
-        'Thom Browne',
-        'Popular Science',
-        'Alvin Goldfarb',
-        'Xbox',
-        'Wings and Horns',
-        'Nike',
-        'Microsoft Surface',
-        'Plant Paper',
-        'Columbia Sportswear',
-        'Autre Magazine',
-        'MTV',
-        'GQ',
-        'INC Magazine',
-        'Self Magazine',
-        'Faris Jewelry',
-        'Barneys',
-        'See Kai Run',
-        'TomboyX',
-        'Olderbrother',
-        'Barneys',
-        'Totokaelo',
-        'Baby and Co',
-        'Grey Matters',
-        'Girlfriend Collective',
-        'Labucq',
-        'Reebok'
-    ];
+    const clients = ['Organic by John Patrick', 'Caron Callahan', 'Thom Browne', 'Popular Science', 'Alvin Goldfarb', 'Xbox', 'Wings and Horns', 'Nike', 'Microsoft Surface', 'Plant Paper', 'Columbia Sportswear', 'Autre Magazine', 'MTV', 'GQ', 'INC Magazine', 'Self Magazine', 'Faris Jewelry', 'Barneys', 'See Kai Run', 'TomboyX', 'Olderbrother', 'Barneys', 'Totokaelo', 'Baby and Co', 'Grey Matters', 'Girlfriend Collective', 'Labucq', 'Reebok'];
 
     return (
-        <div>
+        <Wrapper>
             <h2>Client List</h2>
-            <ul style={{ display: "flex", flexDirection: "column" }}>
+            <List>
                 {clients.map((client, index) => (
-                    <li key={index} style={{ display: "block" }}>{client}</li>
+                    <ListItem key={index}>{client}</ListItem>
                 ))}
-            </ul>
-        </div>
+            </List>
+        </Wrapper>
     );
 };
 
