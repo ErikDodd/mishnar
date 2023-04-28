e8e7ae3a7be376937dbc46efe3de81de3f472c3b
Erik M. Dodd <emdodd27@gmail.com>
2023-04-28 12:16:40 -0700

    Working on Bio/Contact page. Still need to adjust to center the image/text

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,23 +1,19 @@
 import React from 'react';
 
 const Contact = () => {
-    const divStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        position: 'absolute',
-        top: '150px',
-    };
-
+   
 
     return (
-        <div style={divStyle}>
+        <div className="bio-contact-wrapper">
             <h2>BIO/CONTACT</h2>
-            <img src="assets/michele.jpg" alt="A beautiful photo" style={{ width: '25%', height: 'auto', marginTop: '110px' }} />
-            <p id="bio">Michele 'Mish' Andrews is an art, style, and creative director, fueled by her passion and dedication for her clients and art. Her services include storytelling through creative development, brand direction, campaign, video, and social media.</p>
+            <div className="bio-wrapper">
+                <img class="bio-image" src="assets/michele.jpg" alt="A beautiful photo"/>
+                    <p class="bio-text">Michele 'Mish' Andrews is an art, style, and creative director, fueled by her passion and dedication for her clients and art. Her services include storytelling through creative development, brand direction, campaign, video, and social media.</p>
+            </div>
             <a href="mailto:michele.e.andrews@gmail.com">Contact Michele</a>
             <a href="https://www.instagram.com/mishnar/">Visit Michele's Instagram</a>
         </div>
+
     );
 };
 
