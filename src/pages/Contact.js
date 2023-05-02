ba9e1ec7c503ba84560e4f63b3f3a9e5f1025681
Erik M. Dodd <emdodd27@gmail.com>
2023-05-02 09:21:36 -0700

    centered div and text on the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,8 +10,8 @@ const Contact = () => {
                 <img class="bio-image" src="assets/michele.jpg" alt="A beautiful photo"/>
                     <p class="bio-text">Michele 'Mish' Andrews is an art, style, and creative director, fueled by her passion and dedication for her clients and art. Her services include storytelling through creative development, brand direction, campaign, video, and social media.</p>
             </div>
-            <a href="mailto:michele.e.andrews@gmail.com">Contact Michele</a>
-            <a href="https://www.instagram.com/mishnar/">Visit Michele's Instagram</a>
+            <a className="email" href="mailto:michele.e.andrews@gmail.com">Contact Michele</a>
+            <a className="instagram" href="https://www.instagram.com/mishnar/">Visit Michele's Instagram</a>
         </div>
 
     );
