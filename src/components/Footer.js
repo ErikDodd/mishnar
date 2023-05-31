b5dd8abfc90fe52b1784740cf98a64c6bdd4cb11
Erik M. Dodd <emdodd27@gmail.com>
2023-05-30 22:19:38 -0700

    Updated copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,6 +14,7 @@ const Footer = ({ bottomElementHeight }) => {
             <h3>STYLING</h3>
             <h3>CREATIVE DIRECTION</h3>
             <h3>ART DIRECTION</h3>
+            <p>© Erik Dodd 2023</p>
         </div>
     );
 };
